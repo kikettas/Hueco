3d122122ea188f9bf1b1e37c579b7ca82d00d079
Enrique del Pozo Gómez <enrique0703@gmail.com>
2017-02-03 21:26:49 +0100

    - Fixed spelling issue on AppDelegate

--- InfiniteLoopers/InfiniteLoopers/AppDelegate.swift
+++ InfiniteLoopers/InfiniteLoopers/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FIRApp.configure()
-        loadFirsView()
+        loadFirstView()
 
         return true
     }
@@ -44,7 +44,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-    func loadFirsView(){
+    func loadFirstView(){
         let launchViewController = LaunchScreenV(nibName: nil, bundle: nil)
         
         self.window = UIWindow(frame: UIScreen.main.bounds)
